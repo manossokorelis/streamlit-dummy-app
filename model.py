3a64c6be46404a30dd5a60b7aa5b99030c099a1c
Emmanouil Sokorelis <sokorelis.manos@gmail.com>
2025-05-10 10:51:06 +0300

    Version2.4

--- model.py
+++ model.py
@@ -2,33 +2,7 @@
 
 import torch
 import torch.nn as nn
-
-# CNN Model (same as training script)
-class CNN(nn.Module):
-    def __init__(self):
-        super(CNN, self).__init__()
-        self.model = nn.Sequential(
-            nn.Conv2d(1, 32, 3, 1),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-            nn.Conv2d(32, 64, 3, 1),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-            nn.Flatten(),
-            nn.Linear(64 * 5 * 5, 128),
-            nn.ReLU(),
-            nn.Linear(128, 10)
-        )
-
-    def forward(self, x):
-        return self.model(x)
-
-# def load_model(weights_path="mnist_cnn.pth"):
-#     device = torch.device("cpu")
-#     model = CNN()
-#     model.load_state_dict(torch.load(weights_path, map_location=device))
-#     model.eval()
-#     return model
+from torchvision import models 
 
 def load_model():
     # Load a pretrained ResNet18 model
@@ -38,7 +12,7 @@ def load_model():
     # Replace the final fully connected layer to match MNIST (10 classes)
     num_ftrs = model.fc.in_features
     model.fc = nn.Linear(num_ftrs, 10)
-    # Load trained weights if available (optional)
-    # model.load_state_dict(torch.load("resnet_mnist.pth", map_location="cpu"))
     model.eval()
     return model
+
+    
